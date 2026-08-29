9f090486b297b569db43034d39c5f35a2515526b
Tanmay <ajayshigwan7@gmail.com>
2026-08-30 04:18:05 +0530

    Add create session endpoint

--- src/main/java/com/tanmay/study_session_tracker/StudySessionController.java
+++ src/main/java/com/tanmay/study_session_tracker/StudySessionController.java
@@ -1,8 +1,11 @@
 package com.tanmay.study_session_tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,7 +17,19 @@ public class StudySessionController {
         }
 
 
-        
+        @PostMapping("/sessions")
+        public  void addTask(@RequestBody StudySession session){
+            manager.addSession(
+                    session.getSubject(),
+                    session.getDuration(),
+                    session.getStatus()
+            );
+        }
+
+
+
+
+
 
 
 }
