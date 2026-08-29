3bef390e17d9135351e4c08e7036c3c7cdd10e91
Tanmay <ajayshigwan7@gmail.com>
2026-08-30 03:44:13 +0530

    Add study session manager

---
+++ src/main/java/com/tanmay/study_session_tracker/StudySessionManager.java
@@ -0,0 +1,46 @@
+package com.tanmay.study_session_tracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudySessionManager {
+
+    private List<StudySession> sessions;
+
+    public StudySessionManager() {
+
+        sessions = new ArrayList<>();
+
+        sessions.add(new StudySession(
+                "1",
+                "Java",
+                60,
+                "COMPLETED"
+        ));
+
+        sessions.add(new StudySession(
+                "2",
+                "DSA",
+                45,
+                "IN_PROGRESS"
+        ));
+
+        sessions.add(new StudySession(
+                "3",
+                "Spring Boot",
+                90,
+                "COMPLETED"
+        ));
+
+        sessions.add(new StudySession(
+                "4",
+                "Git",
+                30,
+                "TODO"
+        ));
+    }
+
+    public List<StudySession> getSessions() {
+        return sessions;
+    }
+}
